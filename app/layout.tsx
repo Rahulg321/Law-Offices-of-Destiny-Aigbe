a57de9fcf249b073ebf4b10d9f7a86c5cbb9d257
Rahul Gupta <rg5353070@gmail.com>
2024-05-01 19:17:13 +0530

    removed theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/components/theme-provider";
 import clsx from "clsx";
 
 export const metadata: Metadata = {
