f16c601ee081129877d86d26651654e1da184751
Rahul Gupta <rg5353070@gmail.com>
2024-06-04 21:54:30 +0530

    added a new logo

--- slices/HomeHero/index.tsx
+++ slices/HomeHero/index.tsx
@@ -2,7 +2,7 @@
 
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import Logo from "@/public/logo-LB-blue.svg";
+import Logo from "@/public/logoDesign-removebg-preview.png";
 import Image from "next/image";
 import gsap from "gsap";
 import { useRef } from "react";
@@ -45,16 +45,16 @@ const HomeHero = ({ slice }: HomeHeroProps): JSX.Element => {
       className="min-h-screen bg-gray-100 flex items-center justify-center"
       ref={container}
     >
-      <div className="big-container text-center hero-animation">
+      <div className="big-container flex flex-col md:flex-row items-center justify-center text-center hero-animation">
         <Image
           src={Logo}
-          width={200}
-          height={200}
+          width={500}
+          height={500}
           alt="Logo for AIGBE LAW"
           className="mx-auto object-cover"
           priority
         />
-        <span className="text-[clamp(3rem,10vmin,18rem)] hero-animation font-semibold text-mainC">
+        <span className="text-[clamp(3rem,9vmin,16rem)] hero-animation font-semibold text-mainC">
           Relationship <span className="font-extrabold">Focused</span>.
           <br />
           Results <span className="font-extrabold">Driven</span>.
