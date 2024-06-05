32b2334a5b48dabfb93eea0bd374bb9636f565ca
Rahul Gupta <rg5353070@gmail.com>
2024-06-05 23:25:10 +0530

    removed suspense boundary

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -7,6 +7,9 @@ import { components } from "@/slices";
 import BlogPostsIndex from "@/components/BlogPostsIndex";
 import { Suspense } from "react";
 import BlogCardSkeleton from "@/components/skeletons/BlogCardSkeleton";
+import BlogPagination from "@/components/BlogPagination";
+import clsx from "clsx";
+import BlogCard from "@/components/BlogCard";
 
 type Params = { uid: string };
 
@@ -20,32 +23,41 @@ export default async function Page({
   const pageNumber = searchParams.page
     ? parseInt(searchParams.page as string)
     : 1;
+  const client = createClient();
+  const blogposts = await client.getByType("blogpost", {
+    page: pageNumber,
+    pageSize: 20,
+  });
+  console.log("blog posts are ", blogposts);
 
   console.log("page number in server component is", pageNumber);
 
   return (
-    <>
-      <Suspense
-        fallback={
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-12">
-            <BlogCardSkeleton />
-            <BlogCardSkeleton />
-            <BlogCardSkeleton />
-            <BlogCardSkeleton />
-            <BlogCardSkeleton />
-            <BlogCardSkeleton />
-          </div>
-        }
-      >
-        <BlogPostsIndex pageNumber={pageNumber} />
-      </Suspense>
-    </>
+    <div className={clsx("flex flex-col py-6")}>
+      <h1 className="text-mainC text-center">All Posts</h1>
+
+      <div className="grid grid-cols-1 mx-auto mt-6 gap-6 md:gap-8 lg:gap-12">
+        {blogposts.results.map((post, index) => {
+          return (
+            <div key={index}>
+              <BlogCard post={post} />
+            </div>
+          );
+        })}
+      </div>
+      <div className="mt-auto">
+        <BlogPagination
+          totalPages={blogposts.total_pages}
+          currentPage={blogposts.page}
+          itemsPerPage={blogposts.results_per_page}
+          classname=""
+        />
+      </div>
+    </div>
   );
 }
 
-export async function generateMetadata({
-  params,
-}: {
+export async function generateMetadata({}: {
   params: Params;
 }): Promise<Metadata> {
   return {
