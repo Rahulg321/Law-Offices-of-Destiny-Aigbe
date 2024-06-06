5cf74051927e63ff988742e5b77d2617faa15e46
Rahul Gupta <rg5353070@gmail.com>
2024-06-06 15:43:16 +0530

    added pagination one level up

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -20,14 +20,38 @@ export default async function Page({
 }): Promise<JSX.Element> {
   const page = searchParams.page ? parseInt(searchParams.page as string) : 1;
   console.log("page in search params is", page);
+  const client = createClient();
+  const blogposts = await client
+    .getByType("blogpost", {
+      page: Number(page),
+      pageSize: 20,
+    })
+    .catch((err) => console.log(err));
+
+  if (!blogposts) {
+    console.log("could not find blog posts");
+    return notFound();
+  }
 
   return (
     <div className={clsx("flex flex-col py-6")}>
-      <h1 className="text-mainC text-center">All Posts</h1>
-
-      <Suspense key={"pageNumber"} fallback={"loading blogs...."}>
-        <BlogPostsIndex pagenumber={page} />
-      </Suspense>
+      <div className="grid grid-cols-1 mx-auto mt-6 gap-6 md:gap-8 lg:gap-12">
+        {blogposts.results.map((post, index) => {
+          return (
+            <div key={index}>
+              <BlogCard post={post} />
+            </div>
+          );
+        })}
+      </div>
+      <div className="mt-auto">
+        <BlogPagination
+          totalPages={blogposts.total_pages}
+          currentPage={blogposts.page}
+          itemsPerPage={blogposts.results_per_page}
+          classname=""
+        />
+      </div>
     </div>
   );
 }
--- components/BlogPostsIndex.tsx
+++ components/BlogPostsIndex.tsx
@@ -13,37 +13,6 @@ export default async function BlogPostsIndex({
   // const pageNumber = params.get("page") ?? 1;
 
   console.log("page number is ", pagenumber);
-  const client = createClient();
-  const blogposts = await client
-    .getByType("blogpost", {
-      page: Number(pagenumber),
-      pageSize: 20,
-    })
-    .catch((err) => console.log(err));
 
-  if (!blogposts) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className={clsx("flex flex-col py-6")}>
-      <div className="grid grid-cols-1 mx-auto mt-6 gap-6 md:gap-8 lg:gap-12">
-        {blogposts.results.map((post, index) => {
-          return (
-            <div key={index}>
-              <BlogCard post={post} />
-            </div>
-          );
-        })}
-      </div>
-      <div className="mt-auto">
-        <BlogPagination
-          totalPages={blogposts.total_pages}
-          currentPage={blogposts.page}
-          itemsPerPage={blogposts.results_per_page}
-          classname=""
-        />
-      </div>
-    </div>
-  );
+  return <></>;
 }
